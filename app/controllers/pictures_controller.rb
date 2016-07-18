facd118dec3ac90ffbf66b6870ec10c0267d4523
Yassine Rafa <yassine.rafa@outlook.com>
2016-07-18 19:51:50 +0100

    Create all the action for pictures and the new view

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,35 @@
 class PicturesController < ApplicationController
   def index
   end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def created
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to "/"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def find_picture
+    @picture = Picture.find(params[:id])
+  end
+  def picture_params
+    params.require(:picture).permit(:title, :description)
+  end
+
 end
