f477540ca2ed8c0e3ce1b7c95f0cdb8f83259f31
Yassine Rafa <yassine.rafa@outlook.com>
2016-07-19 15:16:55 +0100

    Add Devise and association with pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,11 +9,11 @@ class PicturesController < ApplicationController
   end
 
   def new
-    @picture = Picture.new
+    @picture = current_user.pictures.build
   end
 
   def create
-    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
     if @picture.save
       redirect_to @picture, notice: "Yeeeees, it's posted !"
     else
